6f76ee64564b40384c7d40112a9da9365e321fd2
Jabz01 <jerobaza1@gmail.com>
2023-10-18 11:00:17 -0500

    All functions done, just need to add the darkening effect

--- index.js
+++ index.js
@@ -2,22 +2,74 @@ document.addEventListener("DOMContentLoaded", () => {
     // get the HTML elements.
     const PARENT = document.querySelector("#container");
     const BUTTON_CONTAINER = document.querySelector("#buttonContainer");
-    
+    const BACK_CONTAINER = document.querySelector("#BackToNormalContainer");
+    const TRANSPARENCY_CONTAINER = document.querySelector(".TransparencyContainer");
+    const DARKER_MODE_CONTAINER = document.querySelector("#DarkerModeContainer");
+   
     // Vars for the events of draw and the default worth of the grid, respectively.
     let draw = false;
-    let numberOfSquares = 16;
+    let numberOfSquares = 16; //Default number of the grid
+    let ColorCase = "Black";
+    let TransparencyNumber = 1; 
+    let ButtonClicked = ""; //Var to know what classes add or remove in accordance of what
+    //button was clicked.
+
+
+
     
     // Create a button to clean de grid.
-    const limpiar = document.createElement("button");
-    limpiar.textContent = "CLEAN IT UP";
-    BUTTON_CONTAINER.appendChild(limpiar);
+    const Clear = document.createElement("button");
+    Clear.textContent = "CLEAN IT UP";
+    BUTTON_CONTAINER.appendChild(Clear);
+
+    //Button to eraser in the grid
+    const EraserButton = document.createElement("button");
+    EraserButton.textContent = "ERASER";
+    BUTTON_CONTAINER.appendChild(EraserButton);
     
+    //Button to return to the black color in the grid
+    const ReturnToBlackButton = document.createElement("button");
+    ReturnToBlackButton.textContent = "BACK TO BLACK";
+    ReturnToBlackButton.classList.add("Desactive");
+    BACK_CONTAINER.appendChild(ReturnToBlackButton);
+
+
     // Create a button to change the size of the grid.
     const GridChange = document.createElement("button");
     GridChange.textContent = "CHANGE THE SIZE OF THE GRID";
     BUTTON_CONTAINER.appendChild(GridChange);
     
-    
+    // Create a button to randomize the square's RGB
+    const RandomizeButton = document.createElement("button");
+    RandomizeButton.textContent = "I FEEL LUCKY TODAY";
+    BUTTON_CONTAINER.appendChild(RandomizeButton);
+    RandomizeButton.id = "RandomizeButton";
+
+    //Buttons and Text to change the transparency of each divChildGrid
+    const LessTransparencyButton = document.createElement("button");
+    LessTransparencyButton.textContent = "-";
+    LessTransparencyButton.classList.add("TransparencyButton");
+    const MoreTransparencyButton = document.createElement("button");
+    MoreTransparencyButton.textContent = "+";
+    MoreTransparencyButton.classList.add("TransparencyButton");
+    const transparencyText = document.createElement("h5");
+    transparencyText.textContent = "TRANSPARENCY";
+    TRANSPARENCY_CONTAINER.appendChild(LessTransparencyButton);
+    TRANSPARENCY_CONTAINER.appendChild(transparencyText);
+    TRANSPARENCY_CONTAINER.appendChild(MoreTransparencyButton);
+
+
+    //Darker game button
+    const DarkerPoint = document.createElement("button");
+    DarkerPoint.textContent = "Darker mode";
+    DARKER_MODE_CONTAINER.appendChild(DarkerPoint);
+
+    // FUNCTIONS ---------------------------------------------------------
+
+
+
+
+    //a function that use a bucle for to create a grid (by default 16*16)
     function createGrid() {
         PARENT.innerHTML = '';
         
@@ -31,41 +83,182 @@ document.addEventListener("DOMContentLoaded", () => {
         PARENT.style.setProperty('--number-squares', numberOfSquares)
     }
     createGrid();
-    
-    // Logic of the button "GridChange" to change the size of the grid.
-    GridChange.addEventListener("click", () => {
-        const USER_INPUT = prompt("Give a number to change the Grid");
-        const PARSED_INPUT = parseInt(USER_INPUT);
-        if(!isNaN(PARSED_INPUT) && PARSED_INPUT >= 1 && PARSED_INPUT < 100) {
-            numberOfSquares = PARSED_INPUT;
-            createGrid();
-        } else {
-            alert("Please enter a valid number greater than or equal 1 and lower than 100");
-        };
-    });
-    
-    //Logic of the button "limpiar" to clean the grid.
-    limpiar.addEventListener("click", () => {
+
+
+    //Function to use the logic of the nodeList in the const "GRID" to change the background
+    function ClearFunction() {
         const GRID = document.querySelectorAll(".divChildGrid");
         GRID.forEach((celda1) => {
             if (celda1.style.backgroundColor !== "rgb(255, 255, 255)") {
                 celda1.style.backgroundColor = "#fff";
             };
-        });  
-    });
+        }); 
+    };
+
     
+
+    //use a conditional to change the size of the grid with a min of 1 and a max of 99
+    function GridChangeFunction() {
+        const USER_INPUT = prompt("Give a number to change the Grid");
+        const PARSED_INPUT = parseInt(USER_INPUT);
+        if (USER_INPUT != null) {
+            if(!isNaN(PARSED_INPUT) && PARSED_INPUT >= 1 && PARSED_INPUT < 100) {
+                numberOfSquares = PARSED_INPUT;
+                createGrid();
+            } else {
+                alert("Please enter a valid number greater than or equal 1 and lower than 100");
+            };
+        }
+    };
+
+    //if ColorCase is equal to 0 this function would "paint" the Grid to black
+    function ColorBlack() {
+        if(draw && event.target.classList.contains("divChildGrid")) {
+            event.target.style.backgroundColor = `rgba(0, 0, 0, ${TransparencyNumber})`;
+        };
+    };
+
+    //Function how respond if user click in the button to reduce the 
+    //transparency and viciverse
+    function TransparencyButtonClick(event) {
+        
+        if (event.target === LessTransparencyButton) {
+            TransparencyNumber -= 0.10;
+        }else if (event.target === MoreTransparencyButton) {
+            TransparencyNumber += 0.10;
+        };
+        return TransparencyNumber;
+    };
+
+    //using the methods "round" and "random" in the object math create 
+    //a random rgba number
+    function randomRGB() {
+        const o = Math.round;
+        const r = Math.random;
+        const s = 255;
     
-    // Mouse movement
+        const red = o(r() * s);
+        const green = o(r() * s);
+        const blue = o(r() * s);
+        const alpha = TransparencyNumber;
+    
+        return `rgba(${red}, ${green}, ${blue}, ${alpha})`;        
+    };
+
+    //Every time the button "clear", "Erase", "Randomize", "Return" is pressed the
+    //var "buttonClicked" change according to the button what was pressed to 
+    //active the conditional inside this function
+    function AddOrDesactiveClass() {
+        if(ButtonClicked === "ClearOrReturnToBlackButton") {
+            EraserButton.classList.remove("Desactive");
+            EraserButton.classList.add("TextChange-back");
+            ReturnToBlackButton.classList.remove("TextChange-back")
+            ReturnToBlackButton.classList.add("Desactive");
+
+        } else if (ButtonClicked === "EraserButton") {
+            EraserButton.classList.remove("TextChange-back");
+            EraserButton.classList.add("Desactive");
+            ReturnToBlackButton.classList.remove("Desactive");
+            ReturnToBlackButton.classList.add("TextChange-back");
+
+        } else if (ButtonClicked === "RandomizeButton") {
+            ReturnToBlackButton.classList.add("TextChange-back");
+            ReturnToBlackButton.classList.remove("Desactive");
+            EraserButton.classList.add("TextChange-back");
+        }
+    };
+
+    function AddtransparencyGradually() {
+        
+    }
+
+    // NO MORE FUNCTIONS!!!! --------------------------------------------
+
+
+    // Here's logic --------------------------------------------------------
+
+
+        //Logic of the button "Clear" to clean the grid.
+        Clear.addEventListener("click", () => {
+            ClearFunction();
+            ColorCase = "Black";
+            ButtonClicked = "ClearOrReturnToBlackButton";
+            AddOrDesactiveClass();
+        });
+        
+        // If the user clicks "EraserButton", it sets the ColorCase var
+        // set the background of each DivChildGrid to white if the user
+        // do a mousedown. Immediately the button changes to a "ColorModeButton"
+        // To return to black if the user presses the button again.
+        EraserButton.addEventListener("click", () => {
+            ColorCase = "Eraser";
+            ButtonClicked = "EraserButton";
+            AddOrDesactiveClass();
+        });
+
+
+        // the CSS propierties transform and transition to change the 
+        // visualition of the button.
+        ReturnToBlackButton.addEventListener("click", () => {
+            ColorCase = "Black";
+            ButtonClicked = "ClearOrReturnToBlackButton";
+            TransparencyNumber = 1;
+            AddOrDesactiveClass();
+        });
+        
+        // Logic of the button "GridChange" to change the size of the grid.
+        GridChange.addEventListener("click", () => {
+            GridChangeFunction();
+        });
+        
+
+        //Logic of the button "randomize button"
+        RandomizeButton.addEventListener("click", () => {
+            ColorCase = "RainbowColor";
+            TransparencyNumber = 1;
+            ButtonClicked = "RandomizeButton";
+            AddOrDesactiveClass();
+        });
+
+
+
+
+
+    
+
+        TRANSPARENCY_CONTAINER.addEventListener("click", (event) => {
+            if(event.target === LessTransparencyButton && TransparencyNumber <= 0.10) {
+                alert("The transparency is already 0, you cant substrac anymore.");
+            }else if (event.target === MoreTransparencyButton && TransparencyNumber == 1){
+                alert("The transparency is 1, you can't add more.")
+            }else if(event.target === LessTransparencyButton || event.target === MoreTransparencyButton) {
+                TransparencyButtonClick(event);
+            };
+        });
+
+    
+
+    // Mouse movement ---------------------------------------------------
+
     PARENT.addEventListener("mousedown", () => {
         draw = true;
     });
     PARENT.addEventListener("mouseup", () => {
         draw = false;
     });
+
     PARENT.addEventListener("mousemove", (event) => {
-        if(draw && event.target.classList.contains("divChildGrid")) {
-            event.target.style.backgroundColor = "#000";
-        }
+        if (ColorCase == "Black") {   
+            ColorBlack();
+
+            }else if(ColorCase == "RainbowColor") {
+                if(draw && event.target.classList.contains("divChildGrid")) {
+                    color = randomRGB();
+                    event.target.style.backgroundColor = color;
+                };
+                    } else if (ColorCase == "Eraser" && draw) {
+                        event.target.style.backgroundColor = "#fff"
+                    };
     });
     
 
